eb6845922b971cb1de1f208944f31c6de81f25f1
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-10-09 10:01:34 -0500

    finished array list example and added comments

--- src/main/java/com/pluralsight/ArrayListExample.java
+++ src/main/java/com/pluralsight/ArrayListExample.java
@@ -4,28 +4,34 @@ import java.util.ArrayList;
 
 public class ArrayListExample {
     public static void main(String[] args) {
-        ArrayList<String> students = new ArrayList<>();
-        students.add("Lucas");
-        students.add("Catrina");
-        students.add("Nam Kiet");
-        students.add("Fatinur");
-        students.add("Mario");
-//Output: [Lucas, Catrina, Nam Kiet, Fatinur, Mario]
+        ArrayList<String> studentNames = new ArrayList<>();
+        studentNames.add("Lucas");
+        studentNames.add("Catrina");
+        studentNames.add("Nam Kiet");
+        studentNames.add("Fatinur");
+        studentNames.add("Mario");
 
-        System.out.println(students);
-
-        for (String student: students) {
-            System.out.println(student);
-        }
+        System.out.println(studentNames);
 /* Output:
+[Lucas, Catrina, Nam Kiet, Fatinur, Mario]
+*/
 
+/*
+        studentNames.remove(2)
+
+This will remove the second element in the array
+*/
+
+        for (String studentName: studentNames) {
+            System.out.println(studentName);
+/* Output:
 Lucas
 Catrina
 Nam Kiet
 Fatinur
 Mario
-
- */
+*/
+        }
 
     }
 }
