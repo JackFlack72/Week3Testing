5398003d3985dcb70cfe86e25c9e9746562721d9
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-10-09 09:24:56 -0500

    created array list example

---
+++ src/main/java/com/pluralsight/ArrayListExample.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<String> students = new ArrayList<>();
+        students.add("Lucas");
+        students.add("Catrina");
+        students.add("Nam Kiet");
+        students.add("Fatinur");
+        students.add("Mario");
+
+        System.out.println(students);
+
+
+    }
+}
