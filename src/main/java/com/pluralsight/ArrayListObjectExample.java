9f5a5510de5051830f9aef1d226f559fc9f83a4e
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-10-09 09:47:44 -0500

    finished student and array list object example

--- src/main/java/com/pluralsight/ArrayListObjectExample.java
+++ src/main/java/com/pluralsight/ArrayListObjectExample.java
@@ -1,7 +1,15 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class ArrayListObjectExample {
     public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        Student student1 = new Student(1, "Kanye", "West", "Music");
+        students.add(student1);
+        students.add(new Student(2, "Michael", "Jordan", "NBA"));
+
+        System.out.println(students);
 
     }
 }
--- src/main/java/com/pluralsight/Student.java
+++ src/main/java/com/pluralsight/Student.java
@@ -4,8 +4,39 @@ public class Student {
     private int studentId;
     private String firstName;
     private String lastName;
-    private
+    private String major;
 
+    public Student(int studentId, String firstName, String lastName, String major) {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.major = major;
+    }
 
+    public int getStudentId() {
+        return studentId;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Student{");
+        sb.append("studentId=").append(studentId);
+        sb.append(", firstName='").append(firstName).append('\'');
+        sb.append(", lastName='").append(lastName).append('\'');
+        sb.append(", major='").append(major).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
