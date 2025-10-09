d795cd2ebf6f43c0d9350193cf71bd44ed006e21
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-10-09 12:33:06 -0500

    added local time example

---
+++ src/main/java/com/pluralsight/LocalTimeExample.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+
+import java.time.LocalTime;
+
+public class LocalTimeExample {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.now();
+        System.out.println(time.getMinute());
+    }
+}
